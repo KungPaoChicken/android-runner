eafcde65e3cd4922c5e045299e20bfd1c746237f
KungPaoChicken <c2.lam@student.vu.nl>
2017-06-08 17:55:06 +0200

    Added some convenience methods for adb

--- adb.py
+++ adb.py
@@ -5,3 +5,14 @@ adb = ADB(adb_path='/opt/platform-tools/adb')
 
 def devices():
     return adb.get_devices()
+
+
+def shell(device, cmd):
+    # adb.set_target_by_name(device)
+    return adb.shell_command(cmd)
+
+
+def is_installed(device, name):
+    # adb.set_target_by_name(device)
+    return adb.shell_command('adb shell pm list packages | grep %s' % name)
+
