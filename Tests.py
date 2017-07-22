f386680ae6ab3e06423cd8550505122bce3da7ab
KungPaoChicken <c2.lam@student.vu.nl>
2017-07-22 08:51:26 +0200

    Added depnedency checks for browsers in WebExperiment

--- Tests.py
+++ Tests.py
@@ -1,6 +1,6 @@
 import logging
 import os.path as op
-from util import ConfigError
+# from util import ConfigError # Cyclic dependencies
 
 
 def valid_files(paths):
@@ -16,9 +16,16 @@ def is_integer(number, minimum=0):
     return number
 
 
+def is_string(string):
+    if not isinstance(string, basestring):
+        raise ConfigError('String expected, got %s' % type(string))
+    return string
+
+
 # DOUBLE CHECK
 def check_dependencies(devices, dependencies):
     for device in devices:
         for name, installed in device.is_installed(dependencies).items():
             if not installed:
                 logging.error('Required package %s is not installed' % name)
+                raise ConfigError('Required package %s is not installed' % name)
--- WebExperiment.py
+++ WebExperiment.py
@@ -1,16 +1,15 @@
 from time import sleep
 from Experiment import Experiment
+import Tests
 
 
 class WebExperiment(Experiment):
     def __init__(self, config):
         super(WebExperiment, self).__init__(config)
-        # LOAD AND TEST BROWSER DEPENDENCIES
         # https://stackoverflow.com/a/28151563
         self.browser = 'com.android.chrome'
         self.main_activity = 'com.google.android.apps.chrome.Main'
-        # self.browser = 'org.mozilla.focus'
-        # self.main_activity = 'org.mozilla.focus.activity.MainActivity'
+        Tests.check_dependencies(self.devices, [self.browser])
 
     def before_first_run(self, device, path):
         super(WebExperiment, self).before_first_run(device, path)
@@ -19,7 +18,7 @@ class WebExperiment(Experiment):
     def before_run(self, device, path, run):
         super(WebExperiment, self).before_run(device, path, run)
         device.launch(self.browser, self.main_activity, data_uri=path,
-                      action='android.intent.action.VIEW', from_scratch=True)
+                      action='android.intent.action.VIEW', from_scratch=True, force_stop=True)
         sleep(5)
 
     def after_run(self, device, path, run):
