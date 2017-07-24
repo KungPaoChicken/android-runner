5b0c8cb2a1bf49f8645c0219d25c75998f93a94c
KungPaoChicken <c2.lam@student.vu.nl>
2017-07-24 10:02:25 +0200

    Added logging for ExperimentBuilder to log the path of the JSON

--- ExperimentBuilder.py
+++ ExperimentBuilder.py
@@ -1,8 +1,11 @@
+import logging
 import os.path as op
 from util import load_json
 from Experiment import Experiment
 from WebExperiment import WebExperiment
 from NativeExperiment import NativeExperiment
+
+logger = logging.getLogger('ExperimentBuilder')
 
 
 class ExperimentBuilder(object):
@@ -11,6 +14,7 @@ class ExperimentBuilder(object):
 
     @staticmethod
     def from_json(path):
+        logger.info(path)
         config = load_json(path)
         experiment_type = config['type']
         config['config_dir'] = op.abspath(op.dirname(path))
