35093125c24b10676a25dc4b57637d5241c6c2c1
KungPaoChicken <c2.lam@student.vu.nl>
2017-07-24 10:01:09 +0200

    Merged Exception handling into one block, added a line for verbosity logging

--- __main__.py
+++ __main__.py
@@ -2,33 +2,24 @@ import argparse
 import logging
 import os.path as op
 import sys
-from util import ConfigError
-from Adb import ConnectionError, AdbError
 from ExperimentBuilder import ExperimentBuilder
-
-# %(levelname)s: %(name)s: %(message)s
-logging.basicConfig(stream=sys.stdout, level=logging.INFO, format='%(name)s: %(message)s')
-logger = logging.getLogger(__name__)
 
 
 def main():
+    # %(levelname)s: %(name)s: %(message)s
     parser = argparse.ArgumentParser()
     parser.add_argument('file')
     parser.add_argument('-v', '--verbose', action='store_true')
     args = vars(parser.parse_args())
-    # args['verbose']
+    logging_level = logging.DEBUG if args['verbose'] else logging.INFO
+    logging.basicConfig(stream=sys.stdout, level=logging_level, format='%(name)s: %(message)s')
+    logger = logging.getLogger(__name__)
 
     try:
         experiment = ExperimentBuilder.from_json(op.expanduser(args['file']))
         experiment.start()
-    except ConfigError as e:
-        print(e.message)
-    except ConnectionError as e:
-        print(e.message)
-        exit(0)
-    except AdbError as e:
-        print(e.message)
-        exit(0)
+    except Exception, e:
+        logger.error('%s: %s' % (e.__class__.__name__, e.message))
 
 
 if __name__ == "__main__":
