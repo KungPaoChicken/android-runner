6c2e00e7b4e5b5d9da925d5587e69bc975431b55
randy <randydevries@quicknet.nl>
2019-01-07 01:12:47 -0800

    Moved example scripts into dir

--- example/batterystats/after_run.py
+++
@@ -1,3 +0,0 @@
-def main(device, *args, **kwargs):
-    device.shell('input tap 600 1000') # Prevent the device from sleeping
-    pass
--- example/web/interaction.py
+++
@@ -1,4 +0,0 @@
-def main(device, *args, **kwargs):
-    print('=INTERACTION=')
-    print(device.id)
-    print(device.current_activity())
