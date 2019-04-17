2977a2fd875a27dc72096303af63c48d79f72883
randy <randydevries@quicknet.nl>
2019-04-17 12:34:35 +0200

    Test suite continuation

--- ExperimentRunner/ExperimentFactory.py
+++ ExperimentRunner/ExperimentFactory.py
@@ -3,7 +3,6 @@ import shutil
 import paths
 import os.path as op
 import util
-from PluginTests import PluginTests
 from Progress import Progress
 from Experiment import Experiment
 from NativeExperiment import NativeExperiment
@@ -23,13 +22,11 @@ class ExperimentFactory(object):
         shutil.copy(path, op.join(paths.OUTPUT_DIR, 'config.json'))
         config = util.load_json(path)
         experiment_type = config['type']
-        if progress is None and not experiment_type == 'test':
+        if progress is None:
             progress = Progress(config_file=path, config=config, load_progress=False)
         if experiment_type == 'native':
             return NativeExperiment(config, progress)
         elif experiment_type == 'web':
             return WebExperiment(config, progress)
-        elif experiment_type == 'test':
-            return PluginTests(config)
         else:
             return Experiment(config, progress)
