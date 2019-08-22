b36c99d05fafb3ae3c802694c16188cdb1210c7f
randy <randydevries@quicknet.nl>
2019-08-22 22:22:06 +0200

    Updated to match S2 Git, code restructure for testing

--- ExperimentRunner/Plugins/Batterystats.py
+++ ExperimentRunner/Plugins/Batterystats.py
@@ -4,10 +4,9 @@ import time
 import csv
 import json
 import errno
-import Parser
-
+import BatterystatsParser
+import subprocess
 from collections import OrderedDict
-from subprocess import Popen
 from Profiler import Profiler
 
 
@@ -21,7 +20,8 @@ class Batterystats(Profiler):
         self.cleanup = config.get('cleanup')
 
         # "config" only passes the fields under "profilers", so config.json is loaded again for the fields below
-        config_file = self.load_json(op.join(self.paths.CONFIG_DIR, 'config.json'))
+	#FIX
+        config_file = self.load_json(op.join(self.paths["CONFIG_DIR"], self.paths['ORIGINAL_CONFIG_DIR']))
         self.type = config_file['type']
         self.systrace = config_file.get('systrace_path', 'systrace')
         self.powerprofile = config_file['powerprofile_path']
@@ -47,10 +47,11 @@ class Batterystats(Profiler):
             app = 'com.android.chrome'
 
         # Create files on system
-        systrace_file = '{}systrace_{}_{}.html'.format(self.output_dir, device.id, time.strftime('%Y.%m.%d_%H%M%S'))
-        logcat_file = '{}logcat_{}_{}.txt'.format(self.output_dir, device.id, time.strftime('%Y.%m.%d_%H%M%S'))
+        systrace_file = op.join(self.output_dir, 'systrace_{}_{}.html'.format(device.id, time.strftime('%Y.%m.%d_%H%M%S')))
+        logcat_file = op.join(self.output_dir, 'logcat_{}_{}.txt'.format(device.id, time.strftime('%Y.%m.%d_%H%M%S')))
         batterystats_file = op.join(self.output_dir, 'batterystats_history_{}_{}.txt'.format(device.id, time.strftime(
             '%Y.%m.%d_%H%M%S')))
+        print(batterystats_file)
         results_file_name = 'results_{}_{}.csv'.format(device.id, time.strftime('%Y.%m.%d_%H%M%S'))
         results_file = op.join(self.output_dir, results_file_name)
 
@@ -61,51 +62,66 @@ class Batterystats(Profiler):
         """Runs the systrace method for self.duration seconds in a separate thread"""
         # TODO: Check if 'systrace freq idle' is supported by the device
         global sysproc
-        sysproc = Popen('{} freq idle -e {} -a {} -t {} -b 50000 -o {}'.format
-                        (self.systrace, device.id, app, int(self.duration + 5), systrace_file), shell=True)
+        sysproc = subprocess.Popen('{} freq idle -e {} -a {} -t {} -o {}'.format(self.systrace, device.id, app, int(self.duration + 5), systrace_file), shell=True)
+#FIX
 
     def stop_profiling(self, device, **kwargs):
         self.profile = False
 
-    def collect_results(self, device):
-        # Pull logcat file from device
+    # Pull logcat file from device
+    def pull_logcat(self, device):
         device.shell('logcat -f /mnt/sdcard/logcat.txt -d')
         device.pull('/mnt/sdcard/logcat.txt', logcat_file)
         device.shell('rm -f /mnt/sdcard/logcat.txt')
 
-        # Get BatteryStats data
+    # Get BatteryStats data
+    def get_batterystats_results(self, device):
         with open(batterystats_file, 'w+') as f:
             f.write(device.shell('dumpsys batterystats --history'))
-        batterystats_results = Parser.parse_batterystats(app, batterystats_file, self.powerprofile)
+        batterystats_results = BatterystatsParser.parse_batterystats(app, batterystats_file, self.powerprofile)
+        return batterystats_results
 
-        # Estimate total consumption
-        # charge is given in mAh
+    # Estimate total consumption, charge is given in mAh, volt in mV
+    def get_consumed_joules(self, device):
         charge = device.shell('dumpsys batterystats | grep "Computed drain:"').split(',')[1].split(':')[1]
         volt = device.shell('dumpsys batterystats | grep "volt="').split('volt=')[1].split()[0]
-        energy_consumed_Wh = float(charge) * float(volt) / 1000.0
+        energy_consumed_Wh = float(charge) * float(volt) / 1000000.0
         energy_consumed_J = energy_consumed_Wh * 3600.0
+        return energy_consumed_J
 
+    def get_systrace_results(self, device):
         # Wait for Systrace file finalisation before parsing
         sysproc.wait()
         cores = int(device.shell('cat /proc/cpuinfo | grep processor | wc -l'))
-        systrace_results = Parser.parse_systrace(app, systrace_file, logcat_file, batterystats_file, self.powerprofile,
+        systrace_results = BatterystatsParser.parse_systrace(app, systrace_file, logcat_file, batterystats_file, self.powerprofile,
                                                  cores)
+        return systrace_results
 
+    def write_results(self, batterystats_results, systrace_results, energy_consumed_J):
         with open(results_file, 'w+') as results:
             writer = csv.writer(results, delimiter="\n")
             writer.writerow(
                 ['Start Time (Seconds),End Time (Seconds),Duration (Seconds),Component,Energy Consumption (Joule)'])
             writer.writerow(batterystats_results)
             writer.writerow(systrace_results)
+        # FIX
+        with open(op.join(self.output_dir, 'Joule_{}'.format(results_file_name)), 'w+') as out:
+            out.write('Joule_calculated\n{}\n'.format(energy_consumed_J))
 
-        with open(op.join(self.output_dir, 'Joule_{}'.format(results_file_name))) as out:
-            out.write('Joule calculated\n{}\n'.format(energy_consumed_J))
-
-        # Remove log files
+    def cleanup_logs(self):
         if self.cleanup is True:
+            # Remove log files
             os.remove(systrace_file)
             os.remove(logcat_file)
             os.remove(batterystats_file)
+
+    def collect_results(self, device, path=None):
+        self.pull_logcat(device)
+        batterystats_results = self.get_batterystats_results(device)
+        energy_consumed_J = self.get_consumed_joules(device)
+        systrace_results = self.get_systrace_results(device)
+        self.write_results(batterystats_results, systrace_results, energy_consumed_J)
+        self.cleanup_logs()
 
     def set_output(self, output_dir):
         self.output_dir = output_dir
@@ -128,7 +144,8 @@ class Batterystats(Profiler):
         self.write_to_file(filename, subject_rows)
 
     def aggregate_end(self, data_dir, output_file):
-        rows = self.aggregate(data_dir)
+	#FIX
+        rows = self.aggregate_final(data_dir)
         self.write_to_file(output_file, rows)
 
     def write_to_file(self, filename, rows):
@@ -142,16 +159,17 @@ class Batterystats(Profiler):
             row = {k: v + float(new[k]) for k, v in accum.items() if k not in ['Component', 'count']}
             count = accum['count'] + 1
             return dict(row, **{'count': count})
-
+#FIX
         runs = []
+	runs_total = dict()
         for run_file in [f for f in os.listdir(logs_dir) if os.path.isfile(os.path.join(logs_dir, f))]:
-            if run_file.contains('Joule') and joules:
+            if ('Joule' in run_file) and joules:
                 with open(os.path.join(logs_dir, run_file), 'rb') as run:
                     reader = csv.DictReader(run)
                     init = dict({fn: 0 for fn in reader.fieldnames if fn != 'datetime'}, **{'count': 0})
                     run_total = reduce(add_row, reader, init)
                     runs.append({k: v / run_total['count'] for k, v in run_total.items() if k != 'count'})
-        runs_total = reduce(lambda x, y: {k: v + y[k] for k, v in x.items()}, runs)
+        	runs_total = reduce(lambda x, y: {k: v + y[k] for k, v in x.items()},runs)
         return OrderedDict(
             sorted({'batterystats_' + k: v / len(runs) for k, v in runs_total.items()}.items(), key=lambda x: x[0]))
 
@@ -225,5 +243,3 @@ class FileFormatError(Exception):
 
 class ConfigError(Exception):
     pass
-
-
